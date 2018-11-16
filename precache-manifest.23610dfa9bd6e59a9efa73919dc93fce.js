8b0d6a324e437045faef4b5a31407574d20a2e96
Vlad <vturshyn@gmail.com>
2018-11-16 13:09:44 +0200

    Updates

---
+++ precache-manifest.23610dfa9bd6e59a9efa73919dc93fce.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "196840e609a10dc0ef74",
+    "url": "/react-redux-form-login/static/js/runtime~main.196840e6.js"
+  },
+  {
+    "revision": "be3d9423ea8f3a944a80",
+    "url": "/react-redux-form-login/static/js/main.be3d9423.chunk.js"
+  },
+  {
+    "revision": "ec4a2275e00601502a15",
+    "url": "/react-redux-form-login/static/js/1.ec4a2275.chunk.js"
+  },
+  {
+    "revision": "c93a47801a9bb756304804d1b1564acd",
+    "url": "/react-redux-form-login/index.html"
+  }
+];
